fa6f0a8b8caaf83820ef2fa398be15e1370e3d56
FatherBeefy <markyparkysharky@gmail.com>
2018-06-05 23:46:18 +0200

    Added bootstrap stylings, fixed some javascript errors

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 
 
-function countDown() {
 
-    var count = 3600; //An hour
+    
+var CCOUNT = 3600;
+    
+var t; 
 
-    var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+var count = 3600;
 
-    function timer()
-    {
-    count=count-1;
-    if (count <= 0)
-    {
-        clearInterval(counter);
-        //counter ended, display alert, open twitter
-            alert("Another day closer!");
-            var url = "https://www.twitter.com/"
+function cddisplay() {
+    // displays time in span
+    document.getElementById('timeLeft').innerHTML = Math.round(count / 60);
+   
+}; 
+
+function countdown() {
+    // starts countdown
+    cddisplay();
+    if (count == 0) {
+        // time is up
+        alert("Another day closer!");
+            var url = "https://www.twitter.com/";
             var windowName = "new window";
             window.open(url, windowName, "height=200,width=200");
         return;
-    }
+    } else {
+        count--;
+        t = setTimeout("countdown()", 1000);
+    }return;
+};
 
-    //Time left....
-    console.log(count);
-    document.getElementById("timeLeft").innerHTML = Math.round(count / 60);
-    }
+function cdpause() {
+    // pauses countdown
+    clearTimeout(t);
+};
 
-    document.getElementById("startLine").className = "hidden"; 
-
-    function pause() {
-        clearInterval(counter);
-    
-    }
+function hideButton() {
+    // Greys out button
+    document.getElementById("startButton").className = "btn btn-primary disabled";
+    document.getElementById("startLine").innerHTML = "Kick some bum!";
 }
+
+
+
+
+console.log(count);
